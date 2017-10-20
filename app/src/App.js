aafe9b857d380277f3f5a81c9e825fa77f93af27
santiaago <santiago.arias@red-gate.com>
2017-10-20 17:27:37 +0100

    minimal app

---
+++ app/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+  render() {
+    return <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">ks app</h1>
+        </header>
+        <p className="App-intro">
+          ks app here...
+        </p>
+      </div>
+  }
+}
+
+export default App
