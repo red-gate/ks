5c95d60391a19bcb234d20afccada8071491ad5c
santiaago <santiago.arias@red-gate.com>
2017-10-21 12:56:27 +0100

    ks3 baseline

---
+++ ks3/app/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+  render() {
+    return <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">ks3 app with hot reload</h1>
+        </header>
+        <p className="App-intro">
+          ks3 app here...
+        </p>
+      </div>
+  }
+}
+
+export default App
