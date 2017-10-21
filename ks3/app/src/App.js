54ccfcf8d7e83c07b14a178c6427a38efe843202
santiaago <santiago.arias@red-gate.com>
2017-10-21 16:24:08 +0100

    call backend

--- ks3/app/src/App.js
+++ ks3/app/src/App.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react'
 import './App.css'
+import 'whatwg-fetch'
 
 class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state= { message: 'moon'}
+    this.fetchMessage()
+  }
+
+  fetchMessage(){
+    fetch('/api/hello', {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }).then(response => {
+      return response.json()
+    }).then(json => {
+      this.setState({message: json.message})
+    })
+  }
+
   render() {
     return <div className="App">
         <header className="App-header">
-          <h1 className="App-title">ks3 app with hot reload</h1>
+          <h1 className="App-title">ks3 app</h1>
         </header>
         <p className="App-intro">
           ks3 app here...
+          hello {this.state.message}
         </p>
       </div>
   }
