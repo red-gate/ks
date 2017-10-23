eb94b5778984daa182cdd16a36dd49cb88976dda
santiaago <santiago.arias@red-gate.com>
2017-10-23 09:56:59 +0100

    ks4 baseline

---
+++ ks4/app/src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import './App.css'
+import 'whatwg-fetch'
+
+class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state= { message: 'moon'}
+    this.fetchMessage()
+  }
+
+  fetchMessage(){
+    fetch('/api/hello', {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }).then(response => {
+      return response.json()
+    }).then(json => {
+      this.setState({message: json.message})
+    })
+  }
+
+  render() {
+    return <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">ks4 app</h1>
+        </header>
+        <p className="App-intro">
+          ks4 app here...
+          hello {this.state.message}
+        </p>
+      </div>
+  }
+}
+
+export default App
