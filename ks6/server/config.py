a3a3d2251aef4ae30edc1ddbb44b3fb7566d4777
Chris Heppell <chris.john.heppell@gmail.com>
2018-01-12 09:12:42 +0000

    ks6 walkthrough

---
+++ ks6/server/config.py
@@ -0,0 +1,28 @@
+'ks5 config'
+
+class Config(object):
+    'base class for application configuration details.'
+    SECRET_KEY = 'ks5'
+
+    @staticmethod
+    def init_app(app):
+        'init app'
+        pass
+class DevelopmentConfig(Config):
+    'dev config'
+    DEBUG = True
+    SERVE_STATIC_FILES = False
+    MODE = 'development'
+
+
+class TestingConfig(Config):
+    'test config'
+    DEBUG = False
+    SERVE_STATIC_FILES = True
+    MODE = 'production'
+
+config = {
+    'dev': DevelopmentConfig,
+    'testing': TestingConfig,
+    'default': TestingConfig
+}
