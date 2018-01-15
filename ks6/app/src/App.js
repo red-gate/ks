219ecf440006e1ccb3d2563931ad3f6bba5cfc65
Chris Heppell <chris.john.heppell@gmail.com>
2018-01-15 11:11:45 +0000

    ks6, not ks7

--- ks6/app/src/App.js
+++ ks6/app/src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
   render() {
     return <div className="App">
         <header className="App-header">
-          <h1 className="App-title">ks7 app</h1>
+          <h1 className="App-title">ks6 app</h1>
         </header>
         <p className="App-intro">
           ks6 app here...
